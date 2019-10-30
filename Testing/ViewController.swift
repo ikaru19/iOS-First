700879188cedf8e294c1fd75bb20658a90bc8611
ikaru19 <syafrizalizal17@gmail.com>
2019-10-30 14:26:23 +0700

    Initial Commit

---
+++ Testing/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Testing
+//
+//  Created by Muhammad Syafrizal on 30/10/19.
+//  Copyright © 2019 Muhammad Syafrizal. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
